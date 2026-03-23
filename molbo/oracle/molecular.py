b83a6216a11632544e1e26847b1f9defab2cdfed
wvirany <waltervirany@gmail.com>
2026-03-23 10:16:44 -0400

    Rename mcl1 oracles

--- molbo/oracle/molecular.py
+++ molbo/oracle/molecular.py
@@ -13,7 +13,7 @@ def mcl1_qed(noise_std=0.0):
     return LookupOracle(X_data=X, y_data=y, noise_std=noise_std)
 
 
-def mcl1_docking(noise_std=0.0):
+def mcl1_vina(noise_std=0.0):
     df = load_mcl1_data()
     smiles_list = df["prot_smiles"].to_list()
     X = torch.vstack([smiles_to_morgan_fp(s) for s in smiles_list])
