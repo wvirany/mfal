c7208a1d4ad59e7399bd78e788b945663fbfbdcb
wvirany <waltervirany@gmail.com>
2026-03-23 09:31:58 -0400

    Fix device in sample_init for AnalyticOracle

--- molbo/utils/helpers.py
+++ molbo/utils/helpers.py
@@ -8,7 +8,7 @@ def sample_init(oracle, n_init):
     if isinstance(oracle, AnalyticOracle):
         indices = None
         bounds = oracle.bounds
-        X = torch.rand(n_init, oracle.dim) * (bounds[1] - bounds[0]) + bounds[0]
+        X = torch.rand(n_init, oracle.dim).to(bounds) * (bounds[1] - bounds[0]) + bounds[0]
         y = oracle(X)
     elif isinstance(oracle, LookupOracle):
         indices = np.random.choice(len(oracle.X_data), n_init, replace=False)
