1008cc33d64650c7ad6373e3f662de82bb8624b2
wvirany <waltervirany@gmail.com>
2026-04-17 14:19:11 -0400

    update tiny lib dataset, vina baseline

--- molbo/dataset/tiny_lib.py
+++ molbo/dataset/tiny_lib.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 from typing import List
 
+import pandas as pd
+import torch
+
 from molbo.dataset.base import Dataset
 
 DATA_DIR = Path(__file__).parent.parent.parent / "data"
@@ -8,10 +11,15 @@ DATA_DIR = Path(__file__).parent.parent.parent / "data"
 
 class TinyLibraryDataset(Dataset):
 
-    def __init__(self, path: str = DATA_DIR / "tiny_lib/smiles_30k.txt"):
-        with open(path) as f:
-            self._smiles = [line.strip() for line in f.readlines()]
+    def __init__(self, path: str = DATA_DIR / "tiny_lib/properties_30k.csv"):
+        df = pd.read_csv(path)
+        self._smiles = df["smiles"].tolist()
+        self._columns = {"qed": torch.tensor(df["qed"].values, dtype=torch.float64)}
 
     @property
     def smiles(self) -> List[str]:
         return self._smiles
+
+    @property
+    def columns(self):
+        return self._columns
